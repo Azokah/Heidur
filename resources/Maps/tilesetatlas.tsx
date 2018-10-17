18e5c2249e1fe72f9507dcc2570e8325e73b395c
Azokah <jaimegbonorino@gmail.com>
2018-10-17 16:35:31 -0300

    adding object layer

---
+++ resources/Maps/tilesetatlas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TilesetAtlas" tilewidth="16" tileheight="16" tilecount="280" columns="10">
+ <image source="../Sprites/TilesetAtlas.png" width="160" height="448"/>
+</tileset>
